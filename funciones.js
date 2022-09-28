30a8ffdf64325e754fb69633834134935dd60759
Leslie González <leslie.gonzalez1799@gmail.com>
2022-09-27 20:49:53 -0500

    feat: calculadora virtual

---
+++ funciones.js
@@ -0,0 +1,35 @@
+console.log("Esta es una calculadora");
+
+let inputNum1 = document.getElementById("num1");
+let inputNum2 = document.getElementById("num2");
+let inputResult = document.getElementById("result");
+
+let btnSum = document.getElementById("sum");
+let btnRest = document.getElementById("rest");
+let btnDiv = document.getElementById("div");
+let btnMult = document.getElementById("mult");
+
+const sum = (a,b) => a+b;
+const rest = (a,b) => a-b;
+const div = (a,b) => a/b;
+const mult = (a,b) => a*b;
+
+function suma() {
+    let num1 = parseFloat(inputNum1.value)
+    let num2= parseFloat(inputNum2.value)
+    inputResult.value = sum(num1, num2)
+}
+function resta() {
+    inputResult.value = rest(inputNum1.value, inputNum2.value)
+}
+function division() {
+    inputResult.value = div(inputNum1.value, inputNum2.value)
+}
+function multiplicacion() {
+    inputResult.value = mult(inputNum1.value, inputNum2.value)
+}
+
+btnSum.addEventListener("click", suma);
+btnRest.addEventListener("click", resta);
+btnDiv.addEventListener("click", division);
+btnMult.addEventListener("click", multiplicacion);
